2ea307bd704c942bbf451f49b499cb124ab793e7
Marc Montañés <marc@teclliure.net>
2017-11-21 19:49:33 +0100

    Initial commit

---
+++ app/src/main/java/config/youtube/Config.java
@@ -0,0 +1,14 @@
+package config.youtube;
+
+/**
+ * Created by marc on 20/11/17.
+ */
+
+public final class Config {
+
+    private Config() {
+    }
+
+    public static final String YOUTUBE_API_KEY = "AIzaSyDBNsQ3IaPZnmnuQN3v0s6INTrkTPjw3J0";
+
+}
